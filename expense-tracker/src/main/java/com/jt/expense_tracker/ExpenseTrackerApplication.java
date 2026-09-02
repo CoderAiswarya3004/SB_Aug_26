ca9ee5afee207f4b1da715597a9b50317b060bcf
CoderAiswarya3004 <aiswaryaayaskant3004@gmail.com>
2026-09-02 19:15:29 +0530

    Exspene Tracker

--- expense-tracker/src/main/java/com/jt/expense_tracker/ExpenseTrackerApplication.java
+++ expense-tracker/src/main/java/com/jt/expense_tracker/ExpenseTrackerApplication.java
@@ -2,6 +2,7 @@ package com.jt.expense_tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class ExpenseTrackerApplication {
@@ -9,5 +10,4 @@ public class ExpenseTrackerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ExpenseTrackerApplication.class, args);
 	}
-
 }
--- intro-to-rest/src/main/java/com/jt/intro_to_rest/IntroToRestApplication.java
+++ intro-to-rest/src/main/java/com/jt/intro_to_rest/IntroToRestApplication.java
@@ -9,5 +9,4 @@ public class IntroToRestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(IntroToRestApplication.class, args);
 	}
-
 }
